7bf38e2d713846c22efbfec8b5ec95a04158fe02
Wibiemahardhika22 <wibie2100016081@webmail.uad.ac.id>
2024-04-22 21:15:27 +0700

    feat: membuat tombol pada file home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,21 +8,44 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Homepage"),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.info),
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const InfoPage()));
-            },
+        appBar: AppBar(
+          title: const Text("Homepage"),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.info),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const InfoPage()));
+              },
+            ),
+          ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                            title: const Text("Info"),
+                            content: const Text("Ini adalah contoh dialog"),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: const Text("OK"),
+                              ),
+                            ],
+                          ));
+                },
+                icon: const Icon(Icons.info),
+                label: const Text("Lihat Info"),
+              ),
+            ],
           ),
-        ],
-      ),
-      body: const Center(
-        child: Text("Selamat Datang di EasyMart!"),
-      ),
-    );
+        ));
   }
 }
