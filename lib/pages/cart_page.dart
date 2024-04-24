abc0e300b5ccfe4641767506aa29692cef2532b5
Wibiemahardhika22 <wibie2100016081@webmail.uad.ac.id>
2024-04-24 22:09:08 +0700

    feat: Layout Builder

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -9,9 +9,47 @@ class CartPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Keranjang Belanja"),
       ),
-      body: const Center(
-        child: Text("Ini adalah halaman keranjang belanja"),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth < 600) {
+            return ListView(
+              children: _generateConstraints(),
+            );
+          } else if (constraints.maxWidth < 900) {
+            return GridView.count(
+              crossAxisCount: 2,
+              children: _generateConstraints(),
+            );
+          } else {
+            return GridView.count(
+              crossAxisCount: 4,
+              children: _generateConstraints(),
+            );
+          }
+        },
       ),
     );
   }
+}
+
+List<Widget> _generateConstraints() {
+  return List<Widget>.generate(10, (int index) {
+    return Card(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(height: 8),
+          Text("Product $index"),
+          const SizedBox(height: 8),
+          Text("Rp 100.000"),
+          const SizedBox(height: 8),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("Beli"),
+          ),
+          const SizedBox(height: 8),
+        ],
+      ),
+    );
+  });
 }
