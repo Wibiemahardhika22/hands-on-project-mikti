70b0cc36665ed45fb0bd66792f487f83feb9f68a
Wibiemahardhika22 <wibie2100016081@webmail.uad.ac.id>
2024-04-24 22:04:49 +0700

    feat: Media Query (menampilkan screen width, height, dan orientation)

--- lib/pages/info_page.dart
+++ lib/pages/info_page.dart
@@ -5,13 +5,25 @@ class InfoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    Orientation orientation = MediaQuery.of(context).orientation;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Info"),
-      ),
-      body: const Center(
-        child: Text("Ini adalah halaman info"),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Info"),
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text("Screen Width: ${screenSize.width.toStringAsFixed(2)}"),
+                Text("Screen Height: ${screenSize.height.toStringAsFixed(2)}"),
+                Text("Orientation: $orientation"),
+              ],
+            ),
+          ),
+        ));
   }
 }
