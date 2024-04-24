6fe9b0a7324123c220ad29c4cdd3c62966ea72a0
Wibiemahardhika22 <wibie2100016081@webmail.uad.ac.id>
2024-04-24 20:58:04 +0700

    feat: menambahkan drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,44 +8,83 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Homepage"),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.info),
+      appBar: AppBar(
+        title: const Text("Homepage"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.info),
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const InfoPage()));
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton.icon(
               onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => const InfoPage()));
+                showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                          title: const Text("Info"),
+                          content: const Text("Ini adalah contoh dialog"),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: const Text("OK"),
+                            ),
+                          ],
+                        ));
               },
+              icon: const Icon(Icons.info),
+              label: const Text("Lihat Info"),
             ),
           ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (context) => AlertDialog(
-                            title: const Text("Info"),
-                            content: const Text("Ini adalah contoh dialog"),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                child: const Text("OK"),
-                              ),
-                            ],
-                          ));
-                },
-                icon: const Icon(Icons.info),
-                label: const Text("Lihat Info"),
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: ListView(
+                children: const [
+                  DrawerHeader(
+                    child: Center(child: Text("EasyMart")),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.home),
+                    title: Text("Home"),
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: Icon(Icons.shopping_cart),
+                    title: Text("Cart"),
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: Icon(Icons.account_circle),
+                    title: Text("Account"),
+                  ),
+                  Divider(),
+                ],
               ),
-            ],
-          ),
-        ));
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text("Logout"),
+              onTap: () {
+                // Tambahkan fungsi logout di sini
+              },
+            ),
+            const SizedBox(height: 18),
+          ],
+        ),
+      ),
+    );
   }
 }
