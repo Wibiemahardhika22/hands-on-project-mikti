72a122d0f6356bca5345579647b56643e201a59d
Wibiemahardhika22 <wibie2100016081@webmail.uad.ac.id>
2024-05-13 21:00:13 +0700

    feat: Positioned Center

--- lib/pages/info_page.dart
+++ lib/pages/info_page.dart
@@ -8,48 +8,74 @@ class InfoPage extends StatefulWidget {
 }
 
 class _InfoPageState extends State<InfoPage> {
+  final double boxSize = 150;
+
   int numOfSingleTap = 0;
   int numOfDoubleTap = 0;
   int numOfLongPress = 0;
+
+  double posX = 0.0;
+  double posY = 0.0;
+
+  void centerPosition(BuildContext context) {
+    posX = (MediaQuery.of(context).size.width - boxSize) / 2;
+    posY = (MediaQuery.of(context).size.height - boxSize) / 2 - 30;
+
+    setState(() {
+      posX = posX;
+      posY = posY;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     // Size screenSize = MediaQuery.of(context).size;
     // Orientation orientation = MediaQuery.of(context).orientation;
+    if (posX == 0) {
+      centerPosition(context);
+    }
 
     return Scaffold(
       appBar: AppBar(
         title: const Text("Info"),
       ),
-      body: Center(
-        child: GestureDetector(
-          onTap: () {
-            setState(() {
-              numOfSingleTap++;
-            });
-          },
-          onDoubleTap: () {
-            setState(() {
-              numOfDoubleTap++;
-            });
-          },
-          onLongPress: () {
-            setState(() {
-              numOfLongPress++;
-            });
-          },
-          child: Container(
-            width: 150,
-            height: 150,
-            color: Colors.amber,
+      body: Stack(
+        children: [
+          Positioned(
+            top: posY,
+            left: posX,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  numOfSingleTap++;
+                });
+              },
+              onDoubleTap: () {
+                setState(() {
+                  numOfDoubleTap++;
+                });
+              },
+              onLongPress: () {
+                setState(() {
+                  numOfLongPress++;
+                });
+              },
+              child: Container(
+                width: boxSize,
+                height: boxSize,
+                color: Colors.amber,
+              ),
+            ),
           ),
-        ),
+        ],
       ),
       bottomNavigationBar: Text(
         "Single Taps: $numOfSingleTap - Double Taps: $numOfDoubleTap - Long Press: $numOfLongPress",
-        style: const TextStyle(fontSize: 20), textAlign: TextAlign.center,
+        style: const TextStyle(fontSize: 20),
+        textAlign: TextAlign.center,
       ),
     );
+
     // SafeArea(
     //   child: Center(
     //     child: Column(
